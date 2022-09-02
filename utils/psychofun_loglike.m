5d7978de4f28e2a7a9c1864e997e5280bbf1d984
Luigi Acerbi <luigi.acerbi@gmail.com>
2022-09-02 09:53:47 +0200

    fix: improved notation, added slide

--- utils/psychofun_loglike.m
+++ utils/psychofun_loglike.m
@@ -2,12 +2,12 @@ function loglike = psychofun_loglike(theta,data)
 %PSYCHOFUN_LOGLIKE Log-likelihood for psychometric function model.
 
 s_vec = data(:,9); % Stimulus values (signed contrasts)
-r_vec = data(:,6); % Responses
+y_vec = data(:,6); % Responses
 
 % Compute probability of rightward response for each trial
 p_right = psychofun(theta,s_vec);
 
 % Compute summed log likelihood for all rightwards and leftwards responses
-loglike = sum(log(p_right(r_vec == 1))) + sum(log(1 - p_right(r_vec == -1)));
+loglike = sum(log(p_right(y_vec == 1))) + sum(log(1 - p_right(y_vec == -1)));
 
 end
